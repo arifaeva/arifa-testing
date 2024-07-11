990e8bc46165cb0b17693476612678a3a464e65b
arifaeva <arifa.eva.ixa@gmail.com>
2024-07-12 01:39:12 +0700

    keep calm and commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
+import "@/styles/globals.css";
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../../public/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
